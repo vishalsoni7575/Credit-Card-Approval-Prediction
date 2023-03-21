9bb28ce1ec17e76e914d93728bca0e36c0ad22d4
Vishal Soni <110352976+vishalsoni7575@users.noreply.github.com>
2023-03-21 10:28:42 +0530

    Add files via upload

--- Capstone_SQL_Project.sql
+++ Capstone_SQL_Project.sql
@@ -1,7 +1,9 @@
 
 use credit_card;
+select * from credit_card_sql;
 
-#Group the customers based on their income type and find the average of their annual income.
+
+#***Group the customers based on their income type and find the average of their annual income***
 select Type_Income, 
 		count(Ind_ID) as "Total Customers" ,  ## Counting the total customers aliasing as "Total Customers"
 		round(avg(Annual_income),2) Average_Income  ## Average of Annual Income aliasing as "Average_Income"
@@ -10,22 +12,22 @@ select Type_Income,
 	order by Average_Income desc;   ## Ordering in descending order
 
 
-#Find the female owners of cars and property.
+#***Find the female owners of cars and property***
 select GENDER, 
 		Car_Owner, 
 		Propert_Owner
 		from credit_card_sql
-	where GENDER = 0;   ### 0 represents Female
+	where (GENDER = "F") and ((Car_Owner = "Y") or (Propert_Owner = "Y"));   ### "F" represents Female
 
 
-#Find the male customers who are staying with their families.
+#***Find the male customers who are staying with their families***
 select  ind_id, gender, 
 		family_members
 		from credit_card_sql
-	where (Family_Members is not null) and (gender = 1);   ## combining both Male and family members
+	where (Housing_type = "With parents") and (gender = "M");   ## combining both Male and Housing Type.
 
 
-#Please list the top five people having the highest income. 
+#***Please list the top five people having the highest income***
 select ind_id Customer_ID, 
 		round(annual_income) Annual_income   ## Rounding of Annual Income aliasing as "Annual_income"
 		from credit_card_sql
@@ -33,16 +35,16 @@ select ind_id Customer_ID,
 limit 5;
 
 
-#How many married people are having bad credit.
+#***How many married people are having bad credit***
 select Marital_status,
 		count(ind_id) Customers,  ## Counting the total customers aliasing as "Customers"
         label Bad_Credit  ##  Aliasing as "Bad_Credit"
 		from credit_card_sql
-	where (Marital_status = "Married") and (label = 0)
+	where (Marital_status = "Married") and (label = 1)
 	group  by Marital_status;
 
 
-#What is the highest education level and what is the total count.
+#***What is the highest education level and what is the total count***
 select EDUCATION, 
 		count(EDUCATION) top_education ##  Aliasing as "top_education"
 		from credit_card_sql
@@ -51,12 +53,13 @@ select EDUCATION,
 limit 1;
 
 
-#Between married males and females, who is having more bad credit.
+#***Between married males and females, who is having more bad credit***
 select gender, 
 		count(gender) Count_gender, ##  Aliasing as "Count_gender"
+		Marital_status,
 		label Bad_Credit ##  Aliasing as "Bad_Credit"
 		from credit_card_sql
-	where label = 0  ## Bad_Credit is 0
+	where (Marital_status = "Married") and (label = 1)  ## Bad_Credit is 0
 	group by gender
 	order by Count_gender desc;
 
